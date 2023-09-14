0f7eab615f41b78946f62240d04b1aa73ac0c4e0
Ashutosh Sharma <77958573+ashutosh-buku@users.noreply.github.com>
2023-09-15 01:32:32 +0530

    Initial Commit

---
+++ fund/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  fund
+//
+//  Created by Ashutosh Sharma on 15/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fund/fundApp.swift
@@ -0,0 +1,17 @@
+//
+//  fundApp.swift
+//  fund
+//
+//  Created by Ashutosh Sharma on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct fundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
